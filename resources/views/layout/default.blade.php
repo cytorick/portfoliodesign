c892af28b928c1e11c07a8dfdf1606d413231e9b
Rick Visser <rickvisser99@gmail.com>
2023-02-25 01:37:31 +0100

    Changed menu

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -105,7 +105,7 @@
 
                 </div>
                 <div class="flex-none">
-                    <a href="#" class="btn bg-gray-900 hover:bg-black text-white border-none -ml-10">
+                    <a href="#" class="btn bg-gray-900 hover:bg-black text-white border-0 ml-10">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                         </svg>
